6161b637f0960c50d261bdf62c881bc4741e8980
Deepikaaudi <deepika.audi@gmail.com>
2020-04-28 11:56:18 +0530

    Updates

---
+++ precache-manifest.d6ea6a898dd8e38cb3cff84efe3d6ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d0b77a59f05c6dc7b3635a8c6dc756",
+    "url": "/MonsterPage/index.html"
+  },
+  {
+    "revision": "d6350c742d67c9dfca44",
+    "url": "/MonsterPage/static/css/main.f528df1e.chunk.css"
+  },
+  {
+    "revision": "7719e770ac67fb227dd0",
+    "url": "/MonsterPage/static/js/2.e5474451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterPage/static/js/2.e5474451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6350c742d67c9dfca44",
+    "url": "/MonsterPage/static/js/main.81111a8b.chunk.js"
+  },
+  {
+    "revision": "75c272bceac5392ca02d",
+    "url": "/MonsterPage/static/js/runtime-main.c6c1ca8e.js"
+  }
+]);
